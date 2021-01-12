310c76cee6a01ebe42615ec3f962c8a4a4718eeb
Jack Jones <puddingearl@protonmail.com>
2021-01-12 12:51:04 -0600

    Character controls and simple test level

--- godot/src/characters/player/player.gd
+++ godot/src/characters/player/player.gd
@@ -3,11 +3,13 @@ extends KinematicBody2D
 
 const GRAVITY = 9.8
 
-export var speed = 30.0
+export var speed = 100.0
+export var jump_force = 300.0
 
 var velocity = Vector2()
 
 onready var animation = get_node("Animation")
+onready var sprite = get_node("Sprite")
 
 
 func _ready():
@@ -23,13 +25,19 @@ func _physics_process(_delta):
 		velocity.x -= speed
 		if animation.current_animation == "idle":
 			animation.play("walk")
+		if !sprite.flip_h:
+			sprite.flip_h = true
 	elif Input.is_action_pressed("move_right"):
 		velocity.x += speed
 		if animation.current_animation == "idle":
 			animation.play("walk")
+		if sprite.flip_h:
+			sprite.flip_h = false
 	elif animation.current_animation == "walk":
 		animation.play("idle")
 	
-	velocity.y -= GRAVITY
+	velocity.y += GRAVITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y -= jump_force
 	
-	move_and_slide(velocity, Vector2.UP)
+	velocity = move_and_slide(velocity, Vector2.UP)
